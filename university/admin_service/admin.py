06a925e070940b73b34b5149b37bf8c4d077bf22
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-07-19 18:03:12 +0300

    add permissions for admin

--- university/admin_service/admin.py
+++ university/admin_service/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import TrainingDiscipline, DirectionOfTraining
 
-# Register your models here.
+
+@admin.register(TrainingDiscipline)
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'curator', 'direction_code']
+    list_display_links = ['id']
+
+
+@admin.register(DirectionOfTraining)
+class DirectionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id']
