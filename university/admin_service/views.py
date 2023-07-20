aec0bd0f4289ae28f78d1ecbeaca8583856a5c99
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-07-20 07:44:41 +0300

    add views (from last commit)

--- university/admin_service/views.py
+++ university/admin_service/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from django_celery_results.models import TaskResult
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import DirectionOfTraining, TrainingDiscipline
+from service.models import Curator
+from .serializers import TrainingDisciplineSerializer, DirectionOfTrainingSerializer
+from service.serializers import CuratorSerializer
+from .tasks import generate_report
+from celery.result import AsyncResult
 
-# Create your views here.
+
+class StudyDisciplineViewSet(ModelViewSet):
+    queryset = TrainingDiscipline.objects.all()
+    serializer_class = TrainingDisciplineSerializer
+ #   permission_classes = (IsAdminUser,)
+
+
+class CuratorViewSet(ModelViewSet):
+    queryset = Curator.objects.all()
+    serializer_class = CuratorSerializer
+  #  permission_classes = (IsAdminUser,)
+
+
+class StudyDirectionViewSet(ModelViewSet):
+    queryset = DirectionOfTraining.objects.all()
+    serializer_class = DirectionOfTrainingSerializer
+   # permission_classes = (IsAdminUser,)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def start_generate_report(request):
+    task = generate_report.delay()
+    return Response({'message': 'Generation report has been started.', 'task_id': task.id}, status=202)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def show_generate_report_status(request, task_id):
+    task = AsyncResult(task_id)
+    if task.state == 'PENDING':
+        return Response({'message': 'No task found with the given task_id.'}, status=404)
+    else:
+        return Response({'task': task_id, 'status': task.state}, status=200)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_report(request, task_id):
+    task_result = TaskResult.objects.filter(task_id=task_id, status='SUCCESS')
+    if task_result.exists():
+        result = task_result.first().result
+        filename = result.replace('"', '')
+        with open(os.path.join(settings.BASE_DIR, filename), 'rb') as file:
+            report = file.read()
+        response = HttpResponse(report, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = "attachment; filename=UniversityReport.xlsx"
+        return response
+    cur_status = AsyncResult(task_id).state
+    return Response({'message': 'The report has not been generated yet.', 'generation_status': cur_status})
--- university/service/views.py
+++ university/service/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import Student, StudyGroup
+from .serializers import StudentSerializer, StudyGroupSerializer
 
-# Create your views here.
+
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.select_related('group').all()
+    serializer_class = StudentSerializer
+
+    def get_object(self):
+        return get_object_or_404(Student, pk=self.kwargs.get('student_pk'))
+
+    def get_serializer(self, *args, **kwargs):
+        serializer_class = self.get_serializer_class()
+        kwargs['context'] = self.get_serializer_context()
+        kwargs['curator_pk'] = self.kwargs.get('curator_pk', None)
+        return serializer_class(*args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        curator_pk = self.kwargs.get('curator_pk', None)
+        if curator_pk:
+            data = self.get_queryset().filter(group__direction__curator__pk=curator_pk)
+            serialized_data = self.get_serializer(data,  many=True)
+            return Response(serialized_data.data)
+        return super(StudentViewSet, self).list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        curator_pk = self.kwargs.get('curator_pk', None)
+        student_pk = self.kwargs.get('student_pk', None)
+        if curator_pk and student_pk:
+            data = self.get_object()
+            serialized_data = self.get_serializer(data, many=False)
+            return Response(serialized_data.data)
+        return super(StudentViewSet, self).retrieve(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        group = self.get_object().group
+        group.students_count = F('students_count') - 1
+        group.save()
+        group.refresh_from_db()
+        return super(StudentViewSet, self).destroy(request, *args, **kwargs)
+
+
+class StudyGroupViewSet(ModelViewSet):
+    queryset = StudyGroup.objects.select_related('direction').all()
+    serializer_class = StudyGroupSerializer
+
+    def get_serializer_context(self):
+        context = super(StudyGroupViewSet, self).get_serializer_context()
+        context['curator_pk'] = self.kwargs.get('curator_pk', None)
+        return context
+
+    def get_object(self):
+        return get_object_or_404(StudyGroup, pk=self.kwargs.get('group_pk'))
+
+    def list(self, request, *args, **kwargs):
+        curator_pk = self.kwargs.get('curator_pk', None)
+        if curator_pk:
+            data = self.get_queryset().filter(direction__curator__pk=curator_pk)
+            serializer_data = StudyGroupSerializer(data, many=True)
+            return Response(serializer_data.data)
+        return super(StudyGroupViewSet, self).list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        curator_pk = self.kwargs.get('curator_pk', None)
+        group_pk = self.kwargs.get('group_pk', None)
+        if curator_pk and group_pk:
+            data = self.get_object()
+            serializer_data = StudyGroupSerializer(data, many=False)
+            return Response(serializer_data.data)
+        return super(StudyGroupViewSet, self).retrieve(request, *args, **kwargs)
